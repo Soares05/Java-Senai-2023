fca9ebca54fa00c41473c5196ba74223e1df4974
Guilherme Soares <128149473+Soares05@users.noreply.github.com>
2023-04-14 13:50:52 -0300

    Aula01

---
+++ Aula1/Exemplo01.java
@@ -0,0 +1,53 @@
+// Tipos de variáveis em Java
+/*
+ * String -> Armazena texto
+ * char -> Armazena uma única letra
+ * int -> Números inteiros
+ * float -> números decimais
+ * double -> números decimais 
+ * 
+ * 
+ */
+
+ /*Regras para criar variáveis
+  *Toda váriavel DEVE começar com letra minúscula
+  Não pode haver espaço no nome da variável
+  nomeAluno, enderecoCliente, produtoAdquirido 
+
+
+  */
+
+ public class Exemplo01{
+
+public static void main(String[] args) {
+
+    String nome = "Guilherme Soares" ;
+    int idade = 15;
+    boolean chuva = true; // Em variáveis booleanas eu uso true ou false
+    char letra = 'G'; // Todo char DEVE estar com aspas simples  ''
+    double salario = 3550.80;
+    float altura = (float) 1.80;
+
+
+
+
+    System.out.println("Meu nome é: "+nome);
+    System.out.println("Minha idade é: "+idade);
+    System.out.println("Está chovendo? " +chuva);
+    System.out.println("A inicial do meu nome é? "+letra);
+    System.out.println("Meu salário é: "+salario);
+    System.out.println("Eu tenho: "+altura+" metros");
+
+
+
+
+
+}
+
+
+
+ }
+
+
+
+ 
